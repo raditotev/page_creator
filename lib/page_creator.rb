d9828c192b05a89f617f3120256aadf928506920
raditotev <raditotev@gmail.com>
2018-03-04 08:42:18 +0000

    Add PageCreator module

--- lib/page_creator.rb
+++ lib/page_creator.rb
@@ -1,5 +1,7 @@
 require "page_creator/version"
+require "page_creator/pages"
+require "page_creator/site_prism_page"
+require 'active_support/core_ext/string/inflections'
 
 module PageCreator
-  # Your code goes here...
 end
